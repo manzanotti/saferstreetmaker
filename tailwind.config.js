aa1fb7e5258577a7bfc77b208ff0de8379d08d5c
Paul Manzotti <manzo@gorilla-tactics.com>
2022-09-18 17:12:26 +0100

    Attempt to get CSS tree shaking working

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    "./src/**/*.{ts,html}"
+  ],
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
