1ee019cd1c22a48cf8e8b46a007e9222bcdf2061
Paul Manzotti <manzo@gorilla-tactics.com>
2026-09-24 21:32:51 +0100

    refactor: import feature lookup directly in selection

--- src/features/selection/featureSelection.ts
+++ src/features/selection/featureSelection.ts
@@ -2,7 +2,7 @@ import * as L from 'leaflet';
 import { useMapStore } from '../../stores/mapStore';
 import { useSelectionStore, type SelectedMarker } from '../../stores/selectionStore';
 import { pinia } from '../../stores/index';
-import { getFeatureHistoryId } from '../../composables/layers/layerUtils';
+import { getFeatureHistoryId } from '../../composables/layers/featureLookup';
 import { useGroupStore } from '../../stores/groupStore';
 import { useFeatureDeletionStore } from '../../stores/featureDeletionStore';
 import { findFeatureMemberships } from '../groups/featureMemberships';
